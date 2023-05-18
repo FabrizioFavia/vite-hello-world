c1acdb67014206dbf9036a55de24bb995ff90368
FabrizioFavia <personal.favia@gmail.com>
2023-05-18 17:43:11 +0200

    feat(vue): created navbar

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,32 +1,28 @@
 <script>
-import AppTitle from './components/AppTitle.vue'
+import Header from './components/Header.vue'
 import Jumbo from './components/Jumbo.vue'
+import AppTitle from './components/AppTitle.vue'
+
 export default {
-components : {
-  AppTitle,
-  Jumbo
-}
+  components: {
+    Header,
+    Jumbo,
+    AppTitle
+  }
 
 }
 </script>
 
 <template>
-  <AppTitle/>
-  <Jumbo/>
-
+  <Header />
+  <Jumbo />
+  <AppTitle />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+body {
+  width: 100%;
+  height: 100%;
+  background-color: #222;
 }
 </style>
---
+++ my-vue-app/src/components/Header.vue
@@ -0,0 +1,42 @@
+<script>
+
+export default {
+    name: "Header.vue",
+}
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+/* .header{
+width: 100%;
+height: 80px;
+background-color: green;
+display: flex;
+justify-content: center;
+padding: 10px;
+} */
+</style>
